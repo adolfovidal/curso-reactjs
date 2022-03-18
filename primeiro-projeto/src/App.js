3435b235204b1f39f38509a9b272753ce208028e
adolfovidal <adolfosvidal@gmail.com>
2022-03-18 11:36:38 -0300

    update

--- primeiro-projeto/src/App.js
+++ primeiro-projeto/src/App.js
@@ -1,28 +1,16 @@
 //import logo from './logo.svg';
 import './App.css';
-import SayMyName from './components/SayMyName';
-import People from './components/People';
-import Frase from './components/Frase';
-import List from './components/List';
-
+import Evento from './components/Evento';
+import Form from './components/Form';
+ 
 function App() {
-   const name = "Laura"
 
   return (
     <div className='App'>
-      <h1>Testando CSS</h1>
-      <Frase />
-      <Frase />
-      <SayMyName name="Paula" />
-      <SayMyName name="Pingo" />
-      <SayMyName name={name} />
-      <People 
-        name= "Paula" 
-        age="34" 
-        profession="Confectioner" 
-        photo="https://via.placeholder.com/150"
-        />
-        <List/>
+      <h1>Testando Eventos</h1>
+      <Evento numero= "1"/>
+      <Evento numero= "2"/>
+      <Form />
     </div>
   );
 }
